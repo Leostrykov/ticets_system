0002250cdb2b6465d206a01bf2ee903733a3e15f
Leostrykov <98745088+Leostrykov@users.noreply.github.com>
2023-10-26 17:45:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QDate
 import sqlite3
 
 db = sqlite3.connect('ticets_db.sqlite3')
@@ -26,9 +27,17 @@ class LoginPage(QMainWindow):
                                             WHERE login = '%s' and password = '%s';''' %
                                         (self.login.text(), self.password.text())).fetchone()
                 if info_user is not None:
-                    self.close()
-                    self.second_form = MainWindowCashier(info_user)
-                    self.second_form.show()
+                    # 1 это тип пользователя кассир
+                    if info_user[4] == 1:
+                        self.close()
+                        self.second_form = MainWindowCashier(info_user)
+                        self.second_form.show()
+                    # 2 администратор
+                    elif info_user[4] == 2:
+                        self.close()
+                        self.second_form = AdminWindow(info_user)
+                        self.second_form.show()
+
                 else:
                     raise ValueError('Неверный пароль или логин')
             else:
@@ -42,17 +51,19 @@ class MainWindowCashier(QMainWindow):
     def __init__(self, info_user):
         super().__init__()
         uic.loadUi('interfaces/main_window_Cashier.ui', self)
-        self.setMinimumSize(1081, 643)
+        self.setMinimumSize(1081, 684)
         self.centralwidget.setLayout(self.gridLayout)
         self.user_name.setText(info_user[1])
-        self.interface()
+        self.dateEdit.setDate(QDate.currentDate())
+        self.interface('now')
+        self.dateEdit.dateChanged.connect(self.changetDate)
 
-    def interface(self):
+    def interface(self, date):
         # загружаем сеансы
         try:
-            sessions = cur.execute('''SELECT (SELECT name FROM sessions WHERE sessions.id = 
-            sessions_in_cinema.session_id) as name, datetime_start 
-            FROM sessions_in_cinema;''').fetchall()
+            sessions = cur.execute('''SELECT (SELECT name FROM sessions 
+            WHERE sessions.id = sessions_in_cinema.session_id) as name, datetime_start FROM sessions_in_cinema 
+            WHERE DATE(datetime_start) = DATE('%s');''' % date).fetchall()
             for i in sessions:
                 self.list_of_sessions.addItem(f'{i[0]} | {i[1]}')
             self.list_of_sessions.itemSelectionChanged.connect(self.select_session)
@@ -80,6 +91,26 @@ class MainWindowCashier(QMainWindow):
                     self.about.setPlainText(session[2])
         except ValueError:
             self.statusBar().showMessage('Ошибка: не удалось загрузить сеанс')
+
+    def changetDate(self, new_date):
+        self.list_of_sessions.clear()
+        self.interface(new_date.toString("yyyy-MM-dd"))
+
+
+class AdminWindow(QMainWindow):
+    def __init__(self, info_user):
+        super().__init__()
+        self.log_window = None
+        uic.loadUi('interfaces/main_window_Admin.ui', self)
+        self.setMinimumSize(1091, 711)
+        self.inf = info_user
+        self.user_name.setText(f'Привет {info_user[1]}')
+        self.action_exit_2.triggered.connect(self.exit_from_account)
+
+    def exit_from_account(self):
+        self.close()
+        self.log_window = LoginPage()
+        self.log_window.show()
 
 
 if __name__ == '__main__':
